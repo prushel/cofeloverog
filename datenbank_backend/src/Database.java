ebaab60a95ebee36d75c5e837a2cab927dddfe66
prushel <98187763+prushel@users.noreply.github.com>
2023-04-24 08:00:39 +0200

    Projekte neu erstellt
    - Projekt 1 Datenbank & Backend
    - Projekt 2 GUI

---
+++ datenbank_backend/src/Database.java
@@ -0,0 +1,49 @@
+//Java SQL
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DatabaseMetaData;
+public class Database {
+
+    private String host = "jdbc:postgresql://192.168.43.25:5432/cofedb";
+    private String user = "cofeadmin";
+    private String password = "cofe";
+
+    private Connection conn = null;
+
+    public void connect()
+    {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(host, user, password);
+            System.out.println("Success!");
+            conn.setAutoCommit(true);
+
+
+            PreparedStatement stmt =conn.prepareStatement("SELECT * FROM employee");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next())
+            {
+
+                System.out.println(rs.getInt("employee_id"));
+
+            }
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass ().getName ()+": "+e.getMessage ());
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
+}
