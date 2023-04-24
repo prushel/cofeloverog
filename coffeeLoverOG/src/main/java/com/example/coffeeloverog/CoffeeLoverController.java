b4d45939327af5258b057da782b0ef2228436488
Calvin Schrabauer <calvin.schrabauer@sz-ybbs.ac.at>
2023-04-24 08:34:04 +0200

    Zurück zum alten Stand

--- coffeeLoverOG/src/main/java/com/example/coffeeloverog/CoffeeLoverController.java
+++ coffeeLoverOG/src/main/java/com/example/coffeeloverog/CoffeeLoverController.java
@@ -3,7 +3,7 @@ package com.example.coffeeloverog;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CoffeeLoverController {
     @FXML
     private Label welcomeText;
 
